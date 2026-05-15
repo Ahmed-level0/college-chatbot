8b4832f91b419bb5a2344fe854aa050194ec582a
ahmed <ahmedhambuta9@gmail.com>
2026-05-15 03:53:27 +0300

    fix: edited the chroma db path and enhanced the ChatView

--- backend/chatbot/agent/tools/rules_tool.py
+++ backend/chatbot/agent/tools/rules_tool.py
@@ -1,7 +1,12 @@
 from langchain.tools import tool
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import chromadb
+import os
+
 from .config import GOOGLE_API_KEY
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+CHROMA_PATH = os.path.join(BASE_DIR, "chroma_db")
 
 @tool
 def search_rules(query: str) -> str:
@@ -10,31 +15,33 @@ def search_rules(query: str) -> str:
     ...
     """
     try:
-        client = chromadb.PersistentClient(path="./chroma_db")
-        collection = client.get_or_create_collection("university_rules")
-        
-        # Embed query with LangChain
+
+        client = chromadb.PersistentClient(path=CHROMA_PATH)
+
+        collection = client.get_collection("university_rules")
+
         embeddings = GoogleGenerativeAIEmbeddings(
             model="models/gemini-embedding-001",
             google_api_key=GOOGLE_API_KEY
         )
+
         query_embedding = embeddings.embed_query(query)
-        
-        # Search
+
         results = collection.query(
             query_embeddings=[query_embedding],
             n_results=5
         )
-        
+
         if not results["documents"][0]:
             return "No relevant rules found."
-        
+
         formatted = []
+
         for doc, meta in zip(results["documents"][0], results["metadatas"][0]):
             page = meta.get("page", "unknown")
             formatted.append(f"[Page {page}] {doc}")
-        
+
         return "\n\n".join(formatted)
-        
+
     except Exception as e:
         return f"Error searching rules: {str(e)}"
--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from  .agent.agent import chat
 
 # Create your views here.
 class ChatView(APIView):
+
     def post(self, request):
-        prompt = request.data.get('prompt', '')
-        
-        response = chat(prompt)
-        
-        return Response({'response': response})
+        prompt = request.data.get("prompt")
+
+        if not prompt:
+            return Response(
+                {"error": "Prompt is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            response = chat(prompt)
+
+            return Response({
+                "response": response
+            })
+
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
